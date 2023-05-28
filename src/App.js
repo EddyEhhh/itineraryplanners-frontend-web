4396c97f9003268cc4ca2000f25a96700e71a902
Clarissa Toh <clarissatoh03@gmail.com>
2023-05-29 01:00:35 +0800

    integrated tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div
+          class={`h-screen w-full flex items-center justify-center bg-gray-300 flex-col`}
+      >
+        <label class="toggleDarkBtn">
+          <input type="checkbox" />
+          <span class="slideBtnTg round"></span>
+        </label>
+        <div class="max-w-sm rounded overflow-hidden bg-gray-100 p-5 rounded-lg mt-4 text-white dark:bg-gray-900">
+          <img
+              class="w-full"
+              src="https://v1.tailwindcss.com/img/card-top.jpg"
+              alt="Sunset in the mountains"
+          />
+          <div class="px-6 py-4">
+            <div class="text-gray-800 dark:text-gray-200 font-bold text-xl mb-2">
+              The Coldest Sunset
+            </div>
+            <p class="text-gray-800 dark:text-gray-200">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+          </div>
+        </div>
+      </div>
+  )
 }
-
-export default App;
+export default App
