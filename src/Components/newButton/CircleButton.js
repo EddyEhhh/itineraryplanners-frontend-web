1bbed16f44620ca16cf8e578f3c2b9f5fc8d8b12
ashley <ashleyyytjh@gmail.com>
2023-05-30 16:36:20 +0800

    fixed button text

--- src/Components/newButton/CircleButton.js
+++ src/Components/newButton/CircleButton.js
@@ -7,8 +7,8 @@ export default function CircleButton(props) {
     return (
         //flex justify-center items-center w-10 h-10 rounded-full bg-neutral-200 text-neutral-800
         //<div className={`existing-class ${className}`}>
-        <button className = {`flex justify-center items-center rounded-full ${props.colour} text-neutral-800  h-${props.height} w-${props.width}`}>
-            {props.link}
+        <button className = {`flex justify-center items-center rounded-full ${props.colour} text-neutral-800 ${props.height} ${props.width}`}>
+           {props.text}
         </button>
     );
 };
