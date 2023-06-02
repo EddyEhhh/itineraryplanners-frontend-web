5eabbf454a786f372dcd28d7092635187d7ad156
ashley <ashleyyytjh@gmail.com>
2023-06-02 21:11:13 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import React from 'react'
 
 import { ReactComponent as CalendarIcon } from "./icons/calendar.svg";
 import { ReactComponent as DeleteIcon } from "./icons/delete.svg";
-import SmallCircleButton from "./components/CircleButton/SmallCircleButton";
 import LargeButton from "./components/RectangleButton/LargeButton";
-import SmallButton from "./components/RectangleButton/SmallButton";
+import SmallButton from "./components/RectangleButton/SmallButton/SmallButton";
 import MediumButton from "./components/RectangleButton/MediumButton";
 import XLargeButton from "./components/RectangleButton/XLargeButton";
+
 import Calendar from './components/Calendar/Calendar';
 
 function App() {
   return (
     <div>
-          <Calendar></Calendar>
+        <Calendar></Calendar>
 
-    
-        <div className="h-8 w-full flex justify-center items-center">
-          <SmallButton text = "Login" hover = "true" />
+        <div className="outline h-8 w-full flex justify-center items-center">
+          <SmallButton text = "Login"  hover= "true"/>
         </div>
 
         <div className="h-10 w-full flex justify-center items-center">
@@ -29,9 +28,11 @@ function App() {
         </div>
 
         <div className="h-14 w-full flex justify-center items-center">
-          <XLargeButton text = "Delete account" hover = "true" />
+          <XLargeButton text = "Delete account" hover="true" />
         </div>
-        </div>
+
+
+    </div>
 
 
 )
