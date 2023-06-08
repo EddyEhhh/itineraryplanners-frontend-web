a03e5b9d91d0afbf2bad836447097a4ae77bba5c
ashley <ashleyyytjh@gmail.com>
2023-06-08 16:56:46 +0800

    fixed header position to top

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,14 @@ import {Outlet} from "react-router-dom";
 
 const Layout = () => {
   return (
-      <div>
-           <Header></Header>
+      <div className>
+              <Header></Header>
 
-          <div className="bg-primary-white mt-[2px] h-screen z-20">
+
+          <div className="bg-primary-white mt-[2px] h-screen z-0">
               <div className="pl-40 pr-40">
                   <Outlet></Outlet>
               </div>
-
           </div>
 
       </div>
