29eafee84d4c3297f5c06753b05107a2a6d10de9
ashley <ashleyyytjh@gmail.com>
2023-06-08 12:38:15 +0800

    updated login modal

--- src/components/LogInModal/LogInModal.js
+++ src/components/LogInModal/LogInModal.js
@@ -2,8 +2,33 @@ import React from "react";
 import SmallCircleButton from "../CircleButton/SmallCircleButton/SmallCircleButton";
 import InputBox from "../InputBox";
 
+
 function LogInModal (){
-    
+    return (
+        <div>
+            <button
+                data-modal-toggle="modalID"
+                data-modal-show="modalID"
+            >
+                Click me
+            </button>
+
+            <div id = "modalID" className="aria-hidden:true fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div>
+                    <div className="header">
+                        <h1> Log in </h1>
+                        <SmallCircleButton dataModalHide="reset-password-modal" />
+                    </div>
+
+                    <div className="body flex">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        );
+
 }
 
 export default LogInModal;
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,44 +1,44 @@
-
 import {useNavigate} from "react-router-dom";
-import PasswordReset from "../../components/PasswordResetModal/PasswordResetModal";
 import {MediumButton, SmallButton, XLargeButton} from "../../components/RectangleButton/RectangleButton";
 import kr from './kr.jpeg';
+import PasswordResetModal from "../../components/PasswordResetModal/PasswordResetModal";
+import LogInModal from "../../components/LogInModal/LogInModal";
 
 const Landing = () => {
-    const navigate = useNavigate();
     return (
         <div>
-                <div className = "Layout h-10 relative overflow-hidden h-[950px]">
-                    <img className="w-full absolute"  src ={kr} alt = ""></img>
+            <div className = "Layout h-10 relative overflow-hidden h-[950px] ">
+                <img className="w-full absolute"  src ={kr} alt = ""></img>
 
-                    <div className=" origin-center absolute rotate-[65deg] bg-white h-[1000px] w-[1900px] -ml-[700px] mt-[300px] ">
+                <div className=" origin-center absolute rotate-[65deg] bg-white h-[1000px] w-[1900px] -ml-[700px] mt-[300px] ">
+                </div>
+
+                <div className = "m-5 ml-20 absolute font-semibold flex justify-between items-center w-52 text-base">
+                    <div className = "mr-8 ">
+                        <button > X </button>
                     </div>
-
-                    <div className = "m-5 ml-20 absolute font-semibold flex justify-between items-center w-52 text-base">
-                        <div className = "mr-8 ">
-                            <button > X </button>
-                        </div>
-                        <button> Log in </button>
-                        <button> Sign up </button>
-                        <PasswordReset text ="password reset"></PasswordReset>
-                    </div>
-
-
-                    <div className = "absolute flex-col mt-[350px] ml-[150px] mt-[300px]">
-                        <p className = "font-bold text-5xl leading-[3.2rem]"> Going <br></br>somewhere? </p>
-                        <p className = "mt-2 font-semibold text-lg"> Make your next vacation enriching <br/> &
-                            memorable with our itinerary planner</p>
-
-                        <div className="flex items-center h-12 mt-8 w-contain">
-                            <XLargeButton
-                                text = "Start Planning" hover = "" colour = "primary-green" disabled = "" route = "home"
-                            />
-                        </div>
-                    </div>
-
-
+                    <button> Log in </button>
+                    <button> Sign up </button>
 
                 </div>
+
+
+                <div className = "absolute flex-col mt-[350px] ml-[150px] mt-[300px]">
+                    <p className = "font-bold text-5xl leading-[3.2rem]"> Going <br></br>somewhere? </p>
+                    <p className = "mt-2 font-semibold text-lg"> Make your next vacation enriching <br/> &
+                        memorable with our itinerary planner</p>
+
+                    <div className="flex items-center h-12 mt-8 w-contain">
+                        <XLargeButton
+                            text = "Start Planning" hover = "" colour = "primary-green" disabled = "" route = "home"
+                        />
+                    </div>
+
+                    <LogInModal className = "absolute"></LogInModal>
+                    <PasswordResetModal text = "hello"></PasswordResetModal>
+                </div>
+
+            </div>
         </div>
     );
 }
