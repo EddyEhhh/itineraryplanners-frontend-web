6f0ebe67c9eb481d1cc92da67b18d8e579b23fa4
Clarissa Toh <clarissatoh03@gmail.com>
2023-06-09 20:28:19 +0800

    created register modal

--- src/components/LogInModal/LogInModal.js
+++ src/components/LogInModal/LogInModal.js
@@ -13,55 +13,53 @@ function LogInModal() {
       </button>
 
       <div id="log-in-modal" className="hidden relative container">
-        <div>
-          <div className="corner-element">
-            <MediumCircleButton dataModalHide="log-in-modal" />
-          </div>
-          <div id="modal-container" className="container">
-            <div className="space-y-10">
-              <h1 id="welcome-header" className="font-inter">
-                Welcome
-              </h1>
-              <XLargeButton
-                text="Log in with Google"
-                textColour="black"
-                hover="false"
-                disabled=""
-              />
-            </div>
-            <p id="divider">
-              <span>or</span>
-            </p>
-            <InputBox
-              placeholder="Email"
-              type="email"
-              width="w-[382.35px]"
-              warning="Please enter your email address"
-            ></InputBox>
-            <InputBox
-              placeholder="Password"
-              type="password"
-              width="w-[382.35px]"
-              warning="Please enter your password"
-            ></InputBox>
-            <MediumButton
-              text="Forgot Password?"
-              textColour="black"
-              hover=""
-              colour="white"
-              disabled=""
-            ></MediumButton>
+        <div className="corner-element">
+          <MediumCircleButton dataModalHide="log-in-modal" />
+        </div>
+        <div id="login-modal-container" className="container">
+          <div className="space-y-10">
+            <h1 id="welcome-header" className="font-inter">
+              Welcome
+            </h1>
             <XLargeButton
-              text="Log in"
-              textColour="white"
-              colour="primary-green"
-              hover=""
+              text="Log in with Google"
+              textColour="black"
+              hover="false"
               disabled=""
-            ></XLargeButton>
-            <div id="join-us">
-              <p>Not a member?&nbsp;</p>
-              <button>Join us</button>
-            </div>
+            />
+          </div>
+          <p id="divider">
+            <span>or</span>
+          </p>
+          <InputBox
+            placeholder="Email"
+            type="email"
+            width="w-[382.35px]"
+            warning="Please enter your email address"
+          ></InputBox>
+          <InputBox
+            placeholder="Password"
+            type="password"
+            width="w-[382.35px]"
+            warning="Please enter your password"
+          ></InputBox>
+          <MediumButton
+            text="Forgot Password?"
+            textColour="black"
+            hover=""
+            colour="white"
+            disabled=""
+          ></MediumButton>
+          <XLargeButton
+            text="Log in"
+            textColour="white"
+            colour="primary-green"
+            hover=""
+            disabled=""
+          ></XLargeButton>
+          <div id="join-us">
+            <p>Not a member?&nbsp;</p>
+            <button>Join us</button>
           </div>
         </div>
       </div>
--- src/components/RegisterModal/RegisterModal.js
+++ src/components/RegisterModal/RegisterModal.js
@@ -19,9 +19,9 @@ function RegisterModal() {
           <div className="corner-element">
             <MediumCircleButton dataModalHide="register-modal" />
           </div>
-          <div id="modal-container" className="container">
+          <div id="register-modal-container" className="container">
             <div className="space-y-10">
-              <h1 id="welcome-header" className="font-inter">
+              <h1 id="register-header" className="font-inter">
                 Join us to make the most out of your planning.
               </h1>
               <XLargeButton
@@ -34,25 +34,36 @@ function RegisterModal() {
             <p id="divider">
               <span>or</span>
             </p>
-            <InputBox
-              placeholder="Email"
-              type="email"
-              width="w-[382.35px]"
-              warning="Please enter your email address"
-            ></InputBox>
-            <InputBox
-              placeholder="Password"
-              type="password"
-              width="w-[382.35px]"
-              warning="Please enter your password"
-            ></InputBox>
-            <MediumButton
-              text="Forgot Password?"
-              textColour="black"
-              hover=""
-              colour="white"
-              disabled=""
-            ></MediumButton>
+            <div className="space-y-5">
+              <div id="name-fields" className="space-x-5">
+                <InputBox
+                  title="First Name"
+                  placeholder="First Name"
+                  type="text"
+                  width="w-[181px]"
+                ></InputBox>
+                <InputBox
+                  title="Last Name"
+                  placeholder="Last Name"
+                  type="text"
+                  width="w-[181px]"
+                ></InputBox>
+              </div>
+              <InputBox
+                title="Email Address"
+                placeholder="Email"
+                type="email"
+                width="w-[382.35px]"
+                warning="Please enter your email address"
+              ></InputBox>
+              <InputBox
+                title="Create a password"
+                placeholder="Password"
+                type="password"
+                width="w-[382.35px]"
+                warning="Please enter your password"
+              ></InputBox>
+            </div>
             <XLargeButton
               text="Log in"
               textColour="white"
@@ -61,8 +72,8 @@ function RegisterModal() {
               disabled=""
             ></XLargeButton>
             <div id="join-us">
-              <p>Not a member?&nbsp;</p>
-              <button>Join us</button>
+              <p>Already a member?&nbsp;</p>
+              <button>Log in</button>
             </div>
           </div>
         </div>
