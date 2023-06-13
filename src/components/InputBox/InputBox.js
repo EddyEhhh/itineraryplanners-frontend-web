a824feae7ed130c91268918e698537938c520088
ashley <ashleyyytjh@gmail.com>
2023-06-13 20:33:35 +0800

    changed font for input box

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -16,13 +16,13 @@ function InputBox(props) {
           <input
             placeholder = {props.placeholder}
             type = {props.type}
-            className= {`peer ${props.width} h-12 mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-base shadow-sm 
-            placeholder-slate-500 placeholder-opacity-75
+            className= {`peer ${props.width} h-12 mt-1 block px-3 py-2 bg-white border border-slate-300 rounded-md text-base  
+            placeholder-slate-500 placeholder-opacity-50 placeholder:font-light
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500    
             invalid:border-red-500 invalid:text-red-600 invalid:ring-red-500 invalid:ring-1`}
           />
 
-          <p className = "invisible peer-invalid:visible text-red-500 text-sm">
+          <p className = "invisible peer-invalid:visible text-red-500 text-sm font-normal">
             {props.warning}
           </p>
       </div>
