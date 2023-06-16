99b4e9b527d1614d07239a71ecb45f448454c333
Clarissa Toh <clarissatoh03@gmail.com>
2023-06-16 21:04:59 +0800

    updated accounts page

--- src/components/RectangleButton/XLargeButton/XLargeButton.js
+++ src/components/RectangleButton/XLargeButton/XLargeButton.js
@@ -16,8 +16,7 @@ function XLargeButton(props) {
             {`flex items-center justify-center rounded-full text-lg font-semibold 
                 ${props.hover === "true" ? `w-32 h-8  bg-transparent text-${props.textColour} 
                 hover:transition-all ease-in duration-200 hover:${colour} hover:text-white hover:w-72 hover:h-12` 
-                : `w-72 h-12 text-white
-                ${border === "" ? `bg-${colour}`: `bg-transparent border-[1.5px] border-${border} text-slate-900`} 
+                : `w-72 h-12 text-white ${colour}
                 ${props.disabled === "true" ? `opacity-50` : ``}
                 `}
             `}
