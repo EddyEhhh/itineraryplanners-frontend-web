d07f7231f84805b13f4d6d60108a5aa74f23b5d0
Clarissa Toh <clarissatoh03@gmail.com>
2023-06-22 00:20:05 +0800

    created hamburger menu dropdown

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,78 +1,76 @@
-import React, {Suspense} from "react";
-import './Header.scss';
+import React, { Suspense } from "react";
+import "./Header.scss";
 import { useNavigate, useLocation } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 import LangDropDown from "../langDropDown/LangDropDown";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 function Header() {
   const navigate = useNavigate();
   const [langDropDown, setLangDropDown] = useState(false);
 
-  function langDropDownHandler() {
-      setLangDropDown(!langDropDown);
+  const [showHamburgerDropDown, setHamburgerDropDown] = useState(false);
+  const hamburgerDropDownHandler = () => {
+    setHamburgerDropDown(!showHamburgerDropDown);
   }
-    const { t, i18n } = useTranslation('header');
 
-    return (
-      <Suspense>
+  function langDropDownHandler() {
+    setLangDropDown(!langDropDown);
+  }
+  const { t, i18n } = useTranslation("header");
 
-    <div className="MAIN HEADER z-10 sm:justify-center md:justify-between w-full sm:w-full pl-0 pr-0 sm:pl-40 sm:pr-40 sticky top-0 bg-white h-12 flex items-center shadow font-semibold text-sm">
-      <div className="left-header flex items-center justify-around h-12 w-fit gap-4 min-[834px]:visible invisible">
+  return (
+    <Suspense>
+      <div className="MAIN HEADER z-10 sm:justify-center md:justify-between w-full sm:w-full pl-0 pr-0 sm:pl-40 sm:pr-40 sticky top-0 bg-white h-12 flex items-center shadow font-semibold text-sm">
+        <div className="left-header flex items-center justify-around h-12 w-fit gap-4 min-[834px]:visible invisible">
+          <div className="Home-Box h-11 flex items-center ">
+            <HighlightHome></HighlightHome>
+          </div>
 
-        <div className="Home-Box h-11 flex items-center ">
-          <HighlightHome></HighlightHome>
+          <div className="Home-Box h-11 flex items-center">
+            <HighlightMyTrips></HighlightMyTrips>
+          </div>
         </div>
 
-        <div className="Home-Box h-11 flex items-center">
-          <HighlightMyTrips></HighlightMyTrips>
-        </div>
-
-
-      </div>
-
-      <div className="Center-Header flex items-center w-auto justify-end">
-        <div className="Nav-Icon absolute mr-2">
-          <button className="bg-primary-green rounded-full w-7 h-7 flex items-center justify-center">
-            <svg
-              className="w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
-              />
-            </svg>
-          </button>
-        </div>
+        <div className="Center-Header flex items-center w-auto justify-end">
+          <div className="Nav-Icon absolute mr-2">
+            <button className="bg-primary-green rounded-full w-7 h-7 flex items-center justify-center">
+              <svg
+                className="w-4 h-4"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z"
+                />
+              </svg>
+            </button>
+          </div>
 
           <input
-              type="text"
-              placeholder= {t('SearchBarPlaceholder')}
-              className={` 
+            type="text"
+            placeholder={t("SearchBarPlaceholder")}
+            className={` 
                 text-sm h-9 px-5 w-full sm:w-60 rounded-full shadow shadow-outline border-background-gray
                 placeholder-slate-500 placeholder-opacity-75 placeholder:text-center
                 focus:ring-1 focus:border-primary-green
                 `}
           ></input>
+        </div>
 
-      </div>
-
-
-      <div className="Right-Header flex items-center justify-between h-12 w-fit gap-5 min-[834px]:visible invisible">
+        <div className="Right-Header flex items-center justify-between h-12 w-fit gap-5 min-[834px]:visible invisible">
           <button
-              onClick = {langDropDownHandler}
-              className="hover:text-hover-gray">
-              {t('Lang')}
+            onClick={langDropDownHandler}
+            className="hover:text-hover-gray"
+          >
+            {t("Lang")}
           </button>
-          <Suspense>
-              {langDropDown && (<LangDropDown></LangDropDown>)}
-          </Suspense>
+          <Suspense>{langDropDown && <LangDropDown></LangDropDown>}</Suspense>
           <button className="hover:text-hover-gray">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -90,59 +88,139 @@ function Header() {
             </svg>
           </button>
 
-        <button 
-        className="bg-gray-500 w-10 h-10 rounded-full hover:text-hover-gray"
-        onClick={() => navigate("./account")}
-        >
-          A
-        </button>
-
+          <button
+            className="bg-gray-500 w-10 h-10 rounded-full hover:text-hover-gray"
+            onClick={() => navigate("./account")}
+          >
+            A
+          </button>
+        </div>
       </div>
-
-
-    </div>
-
-      </Suspense>
+      <div id="hamburger">
+          <button 
+          id="hamburger-icon"
+          onClick={hamburgerDropDownHandler}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+          { showHamburgerDropDown && (
+            <div className="w-full bg-white h-32 flex flex-col items-center justify-center shadow-lg ease-in-out duration-100">
+              <HighlightHomeDropdown />
+              <HighlightMyTripsDropdown />
+            </div>
+          ) }
+        </div>
+    </Suspense>
   );
 }
 
 function HighlightHome(props) {
-    const { t, i18n } = useTranslation('header');
+  const { t, i18n } = useTranslation("header");
 
-    const location = useLocation();
+  const location = useLocation();
   const navigate = useNavigate();
-    return (
-        <Suspense>
+  return (
+    <Suspense>
       <div>
-          <button className={`${location.pathname === "/home" ? 'underline-button' : 'no-underline-button'}
+        <button
+          className={`${
+            location.pathname === "/home"
+              ? "underline-button"
+              : "no-underline-button"
+          }
             `}
-                  style={{ color: "primary-orange" }}
-                  onClick={() => navigate("/home")}
-          > {t('Home')}
-          </button>
-      </div>
-        </Suspense>
-
-    );
-  }
-
-
-function HighlightMyTrips() {
-    const { t, i18n } = useTranslation('header');
-
-    const location = useLocation();
-  const navigate = useNavigate();
-    return (
-      <div>
-        <button className={` ${location.pathname === "/my-trips" ? 'underline-button' : 'no-underline-button'} `}
-                style={{ color: "primary-orange" }}
-                onClick={() => navigate("/my-trips")}
+          style={{ color: "primary-orange" }}
+          onClick={() => navigate("/home")}
         >
-            {t('Trips')}
+          {" "}
+          {t("Home")}
         </button>
       </div>
-    );
-  }
+    </Suspense>
+  );
+}
+
+function HighlightMyTrips() {
+  const { t, i18n } = useTranslation("header");
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        className={` ${
+          location.pathname === "/my-trips"
+            ? "underline-button"
+            : "no-underline-button"
+        } `}
+        style={{ color: "primary-orange" }}
+        onClick={() => navigate("/my-trips")}
+      >
+        {t("Trips")}
+      </button>
+    </div>
+  );
+}
+
+function HighlightHomeDropdown() {
+  const { t, i18n } = useTranslation("header");
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <Suspense>
+      <div>
+        <button
+          className={`${
+            location.pathname === "/home"
+              ? "fill-button"
+              : "no-fill-button"
+          }
+            `}
+          style={{ color: "primary-orange" }}
+          onClick={() => navigate("/home")}
+        >
+          {" "}
+          {t("Home")}
+        </button>
+      </div>
+    </Suspense>
+  );
+}
+
+function HighlightMyTripsDropdown() {
+  const { t, i18n } = useTranslation("header");
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        className={` ${
+          location.pathname === "/my-trips"
+            ? "fill-button"
+            : "no-fill-button"
+        } `}
+        style={{ color: "primary-orange" }}
+        onClick={() => navigate("/my-trips")}
+      >
+        {t("Trips")}
+      </button>
+    </div>
+  );
+}
 
 // function HighlightCalendar() {
 //   const location = useLocation();
